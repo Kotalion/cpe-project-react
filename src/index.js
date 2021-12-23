d84f2901992a70ab7b4b1b40be087d035b7ab9b0
S.Kasidit <boyboy6719@gmail.com>
2021-12-23 14:24:39 +0700

    add authorize notify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+  } from "react-router-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import RequestNotify from './RequestNotify';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function Apptest() {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	return (
+
+		  <Router>
+			<Routes>
+			  <Route exact path="/" element={<App />} />
+			  <Route exact path="/RequestNotify" element={<RequestNotify />} />
+			</Routes>
+		  </Router>
+	)
+  }
+  
+  ReactDOM.render(<Apptest />, document.getElementById('root'))
+  
