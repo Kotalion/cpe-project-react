18f2213d0f2cdeacf329a05a6a47c62fc32d897d
S.Kasidit <boyboy6719@gmail.com>
2021-12-30 00:08:59 +0700

    test api

--- src/service/api.js
+++ src/service/api.js
@@ -5,10 +5,11 @@ async function auth() {
         const response = await axios.get("https://notify-bot.line.me/oauth/authorize", {
             response_type: "code",
             client_id: "0XzEIwGNTpKlwlW7SbVxoZ",
-            redirect_uri: "https://cpe-project-react.herokuapp.com/",
-            scope: "notify"
+            redirect_uri: "https://cpe-project-react.herokuapp.com/callback",
+            scope: "notify",
+            state: "ABC123456"
         });
-        console.log("This is auth", response);
+        console.log("This is auth", response.data);
         return response;
     } catch (error) {
         console.log(error);
